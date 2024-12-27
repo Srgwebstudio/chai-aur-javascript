cc4438361b103d2fb65e56ef2c9a54240887ec9c
shivam <shivam@developersrg.com>
2024-12-27 16:26:30 +0530

    datatypes

--- test.js
+++ test.js
@@ -1,8 +1,2 @@
 console.log("Hitesh");
 
-console.log("hey i am a new code editor");
-console.log("hey i am a new code editor");
-console.log("hey i am a new code editor");
-console.log("hey i am a new code editor");
-console.log("hey i am a new code editor");
-// console.log("hey i am a new code editor");

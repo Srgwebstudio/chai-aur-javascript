9e8f6878f044956ba827466720cb28352a8ba486
shivam <shivam@developersrg.com>
2024-12-27 16:23:12 +0530

    datatype

---
+++ 02_datatype.js
@@ -0,0 +1,33 @@
+// two documentation to prefer 
+// 1. MDN 
+// 2. tc39.esk
+
+
+// "use strict"; // treat all JS code as newer versions
+
+// alert(3 + 3); // we are using nodejs , not browser
+
+// console.log(3
+//     +
+//     3); // code readibility should be high
+
+// console.log(Shivam);
+
+// Datatype.
+// let name = "Shivam";
+// let age = 20
+// let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint =>
+// string => ""
+// boolean => true/false
+//null => //standalone value
+// undefined => 
+// symbol => unique
+
+// object =>
+    console.log(typeof null); // object
+    console.log(typeof undefined); // undefined
+    
+    
